846506d1a16e40dc3785fcc53a7f3c1a41f734e4
garima-singh27 <65004952+garima-singh27@users.noreply.github.com>
2020-07-19 20:25:36 +0530

    Added DisplayImage component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import DisplayImage from './components/DisplayImage'
 function App() {
   return (
     <div className="App">
-    <Navbar></Navbar>
-      <header className="App-header">
-      </header>
+    {/* <Navbar></Navbar> */}
+      {/* <header className="App-header">
+      </header> */}
+      <body>
+        <DisplayImage></DisplayImage>
+      </body>
     </div>
   );
 }
---
+++ src/Variables/imageVariable.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import WaltonHallB4OU_HomagePainting from '../media/images/WaltonHallB4OU_HomagePainting.jpg'
+import dream_home from '../media/images/dream_home.JPG'
+import nightmare from '../media/images/nightmare.jpg'
+import revisited_sanative from '../media/images/revisited_sanative.jpg'
+import serenity from '../media/images/serenity.jpg'
+import forest from '../media/images/forest.jpg'
+import village_life from '../media/images/village_life.JPG'
+import chaturmuka_basadi from '../media/images/chaturmuka_basadi.JPG'
+import rainy_season from '../media/images/rainy_season.JPG'
+
+
+
+export default  [
+    {
+        'image':WaltonHallB4OU_HomagePainting,
+        'innerBorder':'enable',
+        'OuterColor':{backgroundColor:'#edc297'},
+        'innerBorderColor':{backgroundColor:'#a58769'}
+        
+    },
+    {
+        'image':dream_home,
+        'innerBorder':'enable',
+        'OuterColor':{backgroundColor:'#f9deed'},
+        'innerBorderColor':{backgroundColor:'#c7b1bd'}
+    },
+    {
+        'image':nightmare,
+        'innerBorder':'enable',
+        'OuterColor':{backgroundColor:'#fff6e5'},
+        'innerBorderColor':{backgroundColor:'#94b0c5'}
+    },
+    {
+        'image':revisited_sanative,
+        'innerBorder':'disable',
+        'OuterColor':{backgroundColor:'#999999'},
+        
+    },
+    {
+        'image':serenity,
+        'innerBorder':'enable',
+        'OuterColor':{backgroundColor:'#edc297'},
+        'innerBorderColor':{backgroundColor:'#a58769'}
+    },
+    {
+        'image':forest,
+        'innerBorder':'enable',
+        'OuterColor':{backgroundColor:'#999999'},
+        'innerBorderColor':{backgroundColor:'#e5e5e5'}
+    },
+    {
+        'image':village_life,
+        'innerBorder':'enable',
+        'OuterColor':{backgroundColor:'#fff6e5'},
+        'innerBorderColor':{backgroundColor:'#94b0c5'}
+        
+    },
+    {
+        'image':chaturmuka_basadi,
+        'innerBorder':'enable',
+        'OuterColor':{backgroundColor:'#f0d4d1'},
+        'innerBorderColor':{backgroundColor:'#f0d4d1'}
+    },
+    {
+        'image':rainy_season,
+        'innerBorder':'enable',
+        'OuterColor':{backgroundColor:'#f7cd47'},
+        'innerBorderColor':{backgroundColor:'#6c666d'}
+    }
+]
