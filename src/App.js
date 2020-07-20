16c0a9f47e35629f3e56f27c98a293e230f4d2fb
garima-singh27 <65004952+garima-singh27@users.noreply.github.com>
2020-07-20 21:22:09 +0530

    Revert "Updated image display"
    This reverts commit a5942517e49477bde21b519b729812b14bb7a9dd.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+<<<<<<< Updated upstream
+import DisplayImage from './components/DisplayImage'
+=======
 import ImageCard from './components/ImageCard';
 import bgHome from './media/images/background5.jpg';
 import images from './config/data';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 
+>>>>>>> Stashed changes
 function App() {
   const count=[1,2,0]
   return (
+<<<<<<< Updated upstream
+    <div className="App">
+    {/* <Navbar></Navbar> */}
+      {/* <header className="App-header">
+      </header> */}
+      <body>
+        <DisplayImage></DisplayImage>
+      </body>
+=======
     <div>
       <Box>
         <header style={{
@@ -37,6 +50,7 @@ function App() {
         </Grid>
       </Box>
 
+>>>>>>> Stashed changes
     </div>
   );
 }
