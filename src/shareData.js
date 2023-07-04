7fb30d515eeb451d4e051744adc1ef8fda3074c4
jinraj <38273588+jinraj@users.noreply.github.com>
2023-07-04 12:37:33 +0530

    Update shareData.js

--- src/shareData.js
+++ src/shareData.js
@@ -22,7 +22,50 @@ function ExcelGenerator() {
     const teamsDropdownRange = `'Teams'!$A$2:$A${teams.length + 1}`; // Range of dropdown values
 
     const dataValidation = {
-      t: 'list',
+      t: 'list',import React, { useState } from 'react';
+import ExcelJS from 'exceljs';
+
+function ExcelGenerator() {
+  const [teams, setTeams] = useState([]);
+  const [selectedTeam, setSelectedTeam] = useState('');
+
+  const handleGenerateExcel = () => {
+    const workbook = new ExcelJS.Workbook();
+    
+    const teamsSheet = workbook.addWorksheet('Teams');
+    teamsSheet.getColumn('A').values = [['Teams'], ...teams.map(team => [team])];
+    
+    const mainSheet = workbook.addWorksheet('Main');
+    mainSheet.columns = [
+      { header: 'Country', key: 'country' },
+      { header: 'Teams', key: 'team', dataValidation: { type: 'list', formula1: `Teams!$A$2:$A${teams.length + 1}` } },
+      { header: 'Games', key: 'games' },
+      { header: 'Wins', key: 'wins' }
+    ];
+    
+    workbook.xlsx.writeBuffer().then(buffer => {
+      const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      saveAs(blob, 'teams.xlsx');
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleGenerateExcel}>Generate Excel</button>
+      <br />
+      <br />
+      <select value={selectedTeam} onChange={(e) => setSelectedTeam(e.target.value)}>
+        <option value="">Select a team</option>
+        {teams.map((team, index) => (
+          <option key={index} value={team}>
+            {team}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
       ref: XLSX.utils.encode_range({ s: mainSheetRange.s, e: mainSheetRange.e, c: teamsColumn }),
       formula1: teamsDropdownRange,
     };
