ed78ab3c6a4a5ad209d926e12eb1c08695056f2d
jinraj <38273588+jinraj@users.noreply.github.com>
2023-07-04 11:04:35 +0530

    Update shareData.js

--- src/shareData.js
+++ src/shareData.js
@@ -1,86 +1,49 @@
-import * as ExcelJS from 'exceljs';
+import React, { useState } from 'react';
+import XLSX from 'xlsx';
 
-async function generateTemplate() {
-  const data = {
-    headers: ["name", "age", "gender"],
-    properties: {
-      gender: ["male", "female", "non-binary", "undisclosed", "transgender", "agender", "bigender", "genderqueer", "genderfluid", "two-spirit", "other", "prefer not to say", "not applicable", "unknown", "rather not say", "all"]
-    }
+function ExcelGenerator() {
+  const [teams, setTeams] = useState([]);
+  const [selectedTeam, setSelectedTeam] = useState('');
+
+  const handleGenerateExcel = () => {
+    const workbook = XLSX.utils.book_new();
+
+    const teamsWorksheet = XLSX.utils.aoa_to_sheet([['Teams'], ...teams]);
+    XLSX.utils.book_append_sheet(workbook, teamsWorksheet, 'Teams');
+
+    const mainWorksheet = XLSX.utils.aoa_to_sheet([
+      ['Country', 'Teams', 'Games', 'Wins'],
+    ]);
+    XLSX.utils.book_append_sheet(workbook, mainWorksheet, 'Main');
+
+    // Create data validation for the Teams column in the Main sheet
+    const mainSheetRange = XLSX.utils.decode_range(mainWorksheet['!ref']);
+    const teamsColumn = mainSheetRange.s.c + 1; // Column index of the Teams column
+    const teamsDropdownRange = `'Teams'!$A$2:$A${teams.length + 1}`; // Range of dropdown values
+
+    const dataValidation = {
+      t: 'list',
+      ref: XLSX.utils.encode_range({ s: mainSheetRange.s, e: mainSheetRange.e, c: teamsColumn }),
+      formula1: teamsDropdownRange,
+    };
+    mainWorksheet['!validations'] = [dataValidation];
+
+    XLSX.writeFile(workbook, 'teams.xlsx');
   };
 
-  const workbook = new ExcelJS.Workbook();
-  const sheet = workbook.addWorksheet("Sheet1");
-
-  // Set headers
-  sheet.addRow(data.headers);
-
-  // Add dummy row for table definition
-  const dummyRow = sheet.addRow([]);
-  dummyRow.hidden = true;
-
-  // Add data rows
-  const numRows = 5000;
-  for (let i = 0; i < numRows; i++) {
-    sheet.addRow(["", "", ""]);
-  }
-
-  // Define the range for the table
-  const tableRange = {
-    from: {
-      row: dummyRow.number,
-      column: 1,
-    },
-    to: {
-      row: dummyRow.number,
-      column: data.headers.length,
-    },
-  };
-
-  // Add the table
-  const table = sheet.addTable({
-    name: "MyTable",
-    ref: tableRange,
-    headerRow: true,
-    totalsRow: false,
-    style: {
-      theme: 'TableStyleMedium2',
-    },
-    columns: data.headers.map(header => ({
-      name: header,
-      filterButton: true,
-      style: { font: { bold: true } },
-    })),
-  });
-
-  // Add dropdown validation to the gender column
-  const genderColumnIndex = data.headers.indexOf("gender");
-  const genderProperty = data.properties.gender;
-
-  const dropdownFormula = `=Sheet1!$${String.fromCharCode(64 + genderColumnIndex + 1)}$2:$${String.fromCharCode(64 + genderColumnIndex + 1)}$${genderProperty.length + 1}`;
-
-  const genderColumn = table.getColumn(genderColumnIndex + 1);
-  genderColumn.dataValidation = {
-    type: 'list',
-    formula1: dropdownFormula,
-    showDropDown: true,
-  };
-
-  // Remove dummy row
-  dummyRow.hidden = false;
-  dummyRow.delete();
-
-  // Convert workbook to XLSX format
-  const workbookBuffer = await workbook.xlsx.writeBuffer();
-  const workbookArray = new Uint8Array(workbookBuffer);
-
-  // Save the workbook as XLSX
-  const xlsxFile = new Blob([workbookArray], { type: 'application/octet-stream' });
-  const url = window.URL.createObjectURL(xlsxFile);
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = 'template.xlsx';
-  a.click();
-  window.URL.revokeObjectURL(url);
+  return (
+    <div>
+      <button onClick={handleGenerateExcel}>Generate Excel</button>
+      <br />
+      <br />
+      <select value={selectedTeam} onChange={(e) => setSelectedTeam(e.target.value)}>
+        <option value="">Select a team</option>
+        {teams.map((team, index) => (
+          <option key={index} value={team}>
+            {team}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
-
-generateTemplate();
