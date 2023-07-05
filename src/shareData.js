565553a10317c014394de772f5efc0fb6c9a91ae
jinraj <38273588+jinraj@users.noreply.github.com>
2023-07-05 22:08:10 +0530

    Update shareData.js

--- src/shareData.js
+++ src/shareData.js
@@ -2,7 +2,7 @@ const XLSX = require('xlsx');
 
 function jsonToExcel(data, sheetName, outputFilePath) {
   const workbook = XLSX.utils.book_new();
-  const worksheet = XLSX.utils.aoa_to_sheet([[]]); // Create an empty worksheet
+  const worksheet = XLSX.utils.json_to_sheet(data);
 
   // Convert headers to title case and add spaces between words
   const headers = Object.keys(data[0]);
@@ -13,11 +13,14 @@ function jsonToExcel(data, sheetName, outputFilePath) {
     headers[i] = headers[i][0].toUpperCase() + headers[i].slice(1); // Convert to title case
   }
 
-  // Set the header row with bold style
-  XLSX.utils.sheet_set_array_formulae(worksheet, [{ ref: 'A1:Z1', formula: headers }]);
-
-  // Add the JSON data starting from the second row
-  XLSX.utils.sheet_add_json(worksheet, data, { skipHeader: true, origin: 'A2' });
+  // Make headers bold
+  const headerStyle = { bold: true };
+  XLSX.utils.sheet_add_json(worksheet, [{}], { skipHeader: true, origin: 'A1' });
+  headers.forEach((header, index) => {
+    const cellAddress = XLSX.utils.encode_cell({ r: 0, c: index });
+    worksheet[cellAddress].v = header;
+    worksheet[cellAddress].s = headerStyle;
+  });
 
   // Add the worksheet to the workbook
   XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
