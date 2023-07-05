1cbc3025ca81696c962ffb6b226ecdd469d90ca2
jinraj <38273588+jinraj@users.noreply.github.com>
2023-07-05 21:57:17 +0530

    Update shareData.js

--- src/shareData.js
+++ src/shareData.js
@@ -4,20 +4,24 @@ function jsonToExcel(data, sheetName, outputFilePath) {
   const workbook = XLSX.utils.book_new();
   const worksheet = XLSX.utils.json_to_sheet(data);
 
-  // Convert headers to title case
+  // Convert headers to title case and add spaces between words
   const headers = Object.keys(data[0]);
   for (let i = 0; i < headers.length; i++) {
-    headers[i] = headers[i][0].toUpperCase() + headers[i].slice(1);
+    headers[i] = headers[i]
+      .replace(/([a-z])([A-Z])/g, '$1 $2') // Add space before capital letters
+      .replace(/_/g, ' '); // Replace underscores with spaces
+    headers[i] = headers[i][0].toUpperCase() + headers[i].slice(1); // Convert to title case
   }
 
-  // Make headers bold
-  const headerStyle = { bold: true };
-  XLSX.utils.sheet_add_json(worksheet, [{}], { skipHeader: true, origin: 'A1' });
-  headers.forEach((header, index) => {
-    const cellAddress = XLSX.utils.encode_cell({ r: 0, c: index });
-    worksheet[cellAddress].v = header;
-    worksheet[cellAddress].s = headerStyle;
-  });
+  // Insert headers at the beginning of the worksheet
+  XLSX.utils.sheet_add_aoa(worksheet, [headers], { origin: 'A1' });
+
+  // Set the header row to bold
+  const headerRange = XLSX.utils.decode_range(worksheet['!ref']);
+  for (let col = headerRange.s.c; col <= headerRange.e.c; col++) {
+    const headerCell = XLSX.utils.encode_cell({ r: 0, c: col });
+    worksheet[headerCell].s = { font: { bold: true } };
+  }
 
   // Add the worksheet to the workbook
   XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
